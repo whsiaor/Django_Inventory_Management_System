ef72c1286c7d1f7cdb686a9f9a0dbf1ba5ba5054
whsiaor <whsiaor@gmail.com>
2024-06-27 19:02:58 +1000

    First edition

---
+++ invProject/invApp/views.py
@@ -0,0 +1,42 @@
+from django.shortcuts import render, redirect
+from .forms import ProductForm
+from .models import Product
+
+# Create your views here.
+# Home View
+def home_view(request):
+    return render(request, 'invApp/home.html')
+
+# Create View
+def product_create_view(request):
+    form = ProductForm()
+    if request.method == "POST":
+        form = ProductForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('product_list')
+    return render(request, 'invApp/product_form.html', {'form':form})
+
+# Read View
+def product_list_view(request):
+    products = Product.objects.all()
+    return render(request, 'invApp/product_list.html', {'products':products})
+
+# Update View
+def product_update_view(request, product_id):
+    product = Product.objects.get(product_id=product_id)
+    form = ProductForm()
+    if request.method == "POST":
+        form = ProductForm(request.POST, instance=product)
+        if form.is_valid():
+            form.save()
+            return redirect('product_list')
+    return render(request, 'invApp/product_form.html', {'form':form})
+
+# Delete View
+def product_delete_view(request, product_id):
+    product = Product.objects.get(product_id=product_id)
+    if request.method == "POST":
+        product.delete()
+        return redirect('product_list')
+    return render(request, 'invApp/product_confirm_delete.html', {'product':product})
